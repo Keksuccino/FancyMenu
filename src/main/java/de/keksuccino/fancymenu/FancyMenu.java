2a8b92e03c48c70577de23f42d5b235d818a7af5
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2022-04-25 00:17:29 +0200

    Release v2.6.5

--- src/main/java/de/keksuccino/fancymenu/FancyMenu.java
+++ src/main/java/de/keksuccino/fancymenu/FancyMenu.java
@@ -42,10 +42,10 @@ import org.apache.logging.log4j.Logger;
 @Mod("fancymenu")
 public class FancyMenu {
 
-	public static final String VERSION = "2.6.4";
+	public static final String VERSION = "2.6.5";
 	public static final String MOD_LOADER = "forge";
 
-	public static final Logger LOGGER = LogManager.getLogger();
+	public static final Logger LOGGER = LogManager.getLogger("fancymenu/FancyMenu");
 
 	public static Config config;
 
@@ -108,11 +108,11 @@ public class FancyMenu {
 //				MinecraftForge.EVENT_BUS.register(new Test());
 
 				if (isOptifineCompatibilityMode()) {
-					LOGGER.info("[FANCYMENU] Optifine compatibility mode!");
+					LOGGER.info("Optifine compatibility mode!");
 				}
 
 	    	} else {
-	    		System.out.println("## WARNING ## 'FancyMenu' is a client mod and has no effect when loaded on a server!");
+	    		LOGGER.warn("WARNING: FancyMenu is a client mod and has no effect when loaded on a server!");
 	    	}
 
 		} catch (Exception e) {
--- src/main/java/de/keksuccino/fancymenu/menu/fancy/gameintro/GameIntroEvents.java
+++ src/main/java/de/keksuccino/fancymenu/menu/fancy/gameintro/GameIntroEvents.java
@@ -1,16 +1,25 @@
 package de.keksuccino.fancymenu.menu.fancy.gameintro;
 
 import de.keksuccino.fancymenu.menu.animation.AnimationHandler;
+import de.keksuccino.fancymenu.menu.fancy.MenuCustomization;
 import de.keksuccino.konkrete.rendering.animation.IAnimationRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.MainMenuScreen;
 import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class GameIntroEvents {
+
+	private static boolean titleScreenDisplayed = false;
 	
 	@SubscribeEvent
 	public void onScreenInitPre(GuiScreenEvent.InitGuiEvent.Pre e) {
+		if (e.getGui() instanceof MainMenuScreen) {
+			titleScreenDisplayed = true;
+		} else if (titleScreenDisplayed && MenuCustomization.isValidScreen(e.getGui())) {
+			GameIntroHandler.introDisplayed = true;
+		}
 		if ((e.getGui() instanceof MainMenuScreen) && AnimationHandler.isReady() && !GameIntroHandler.introDisplayed) {
 			IAnimationRenderer intro = GameIntroHandler.getGameIntroAnimation();
 			if (intro != null) {
@@ -18,6 +27,13 @@ public class GameIntroEvents {
 			} else {
 				GameIntroHandler.introDisplayed = true;
 			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onClientTick(TickEvent.ClientTickEvent e) {
+		if (!GameIntroHandler.introDisplayed && (Minecraft.getInstance().world != null)) {
+			GameIntroHandler.introDisplayed = true;
 		}
 	}
 
