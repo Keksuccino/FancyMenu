fa2eae15fab1e14df22d8112f91c6ccec17e2fdb
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2022-07-10 22:13:56 +0200

    Release v2.10.1

--- src/main/java/de/keksuccino/fancymenu/FancyMenu.java
+++ src/main/java/de/keksuccino/fancymenu/FancyMenu.java
@@ -52,7 +52,7 @@ import org.apache.logging.log4j.Logger;
 @Mod("fancymenu")
 public class FancyMenu {
 
-	public static final String VERSION = "2.10.0";
+	public static final String VERSION = "2.10.1";
 	public static final String MOD_LOADER = "forge";
 
 	public static final Logger LOGGER = LogManager.getLogger("fancymenu/FancyMenu");
