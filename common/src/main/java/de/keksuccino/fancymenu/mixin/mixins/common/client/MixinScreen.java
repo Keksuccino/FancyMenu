091036c5dd304f84e82d769cb2627ce814c61c8f
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-01-25 12:12:26 +0100

    ---

--- common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinScreen.java
+++ common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinScreen.java
@@ -5,6 +5,7 @@ import de.keksuccino.fancymenu.customization.ScreenCustomization;
 import de.keksuccino.fancymenu.util.event.acara.EventHandler;
 import de.keksuccino.fancymenu.events.screen.RenderedScreenBackgroundEvent;
 import de.keksuccino.fancymenu.util.rendering.ui.screen.CustomizableScreen;
+import de.keksuccino.fancymenu.util.rendering.ui.widget.NavigatableWidget;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.events.GuiEventListener;
@@ -13,11 +14,14 @@ import net.minecraft.client.gui.screens.Screen;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +33,10 @@ public abstract class MixinScreen implements CustomizableScreen {
 
 	@Unique
 	private final List<GuiEventListener> removeOnInitChildrenFancyMenu = new ArrayList<>();
+	@Unique
+	private boolean nextFocusPath_called_FancyMenu = false;
+
+	@Shadow @Final private List<GuiEventListener> children;
 
 	@Inject(method = "renderBackground", at = @At(value = "RETURN"))
 	private void afterRenderScreenBackgroundFancyMenu(GuiGraphics graphics, CallbackInfo info) {
@@ -41,6 +49,35 @@ public abstract class MixinScreen implements CustomizableScreen {
 		if (Compat.isRRLSLoaded() && (overlay != null) && Compat.isRRLSOverlay(overlay)) {
 			LOGGER.info("[FANCYMENU] Re-initializing screen after init in overlay to fix incompatibility with RemoveReloadingScreen..");
 			ScreenCustomization.reInitCurrentScreen();
+		}
+	}
+
+	@Inject(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/events/AbstractContainerEventHandler;nextFocusPath(Lnet/minecraft/client/gui/navigation/FocusNavigationEvent;)Lnet/minecraft/client/gui/ComponentPath;"))
+	private void beforeNextFocusPathInKeyPressedFancyMenu(int $$0, int $$1, int $$2, CallbackInfoReturnable<Boolean> info) {
+		this.nextFocusPath_called_FancyMenu = true;
+	}
+
+	@Inject(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/events/AbstractContainerEventHandler;nextFocusPath(Lnet/minecraft/client/gui/navigation/FocusNavigationEvent;)Lnet/minecraft/client/gui/ComponentPath;", shift = At.Shift.AFTER))
+	private void afterNextFocusPathInKeyPressedFancyMenu(int $$0, int $$1, int $$2, CallbackInfoReturnable<Boolean> info) {
+		this.nextFocusPath_called_FancyMenu = false;
+	}
+
+	@Inject(method = "setInitialFocus", at = @At("HEAD"))
+	private void beforeSetInitialFocusFancyMenu(GuiEventListener $$0, CallbackInfo info) {
+		this.nextFocusPath_called_FancyMenu = true;
+	}
+
+	@Inject(method = "setInitialFocus", at = @At("RETURN"))
+	private void afterSetInitialFocusFancyMenu(GuiEventListener $$0, CallbackInfo info) {
+		this.nextFocusPath_called_FancyMenu = true;
+	}
+
+	@Inject(method = "children", at = @At("RETURN"), cancellable = true)
+	private void atReturnChildrenFancyMenu(CallbackInfoReturnable<List<? extends GuiEventListener>> info) {
+		if (this.nextFocusPath_called_FancyMenu) {
+			List<GuiEventListener> filtered = new ArrayList<>(this.children);
+			filtered.removeIf(guiEventListener -> (guiEventListener instanceof NavigatableWidget n) && (!n.isFocusable() || !n.isNavigatable()));
+			info.setReturnValue(filtered);
 		}
 	}
 
--- common/src/main/java/de/keksuccino/fancymenu/util/rendering/ui/widget/NavigatableWidget.java
+++ common/src/main/java/de/keksuccino/fancymenu/util/rendering/ui/widget/NavigatableWidget.java
@@ -1,9 +1,10 @@
 package de.keksuccino.fancymenu.util.rendering.ui.widget;
 
 import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.screens.Screen;
 
 /**
- * {@link GuiEventListener}s that implement this interface can control if they should be navigatable and/or focusable.
+ * {@link GuiEventListener}s that implement this interface can control if they should be navigatable and/or focusable in {@link Screen}s.
  */
 public interface NavigatableWidget {
 
