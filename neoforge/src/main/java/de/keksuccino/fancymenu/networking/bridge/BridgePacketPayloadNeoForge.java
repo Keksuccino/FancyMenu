cfa9a651bc51241cf4d7f4e6c9229ee9fe70b717
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-06-21 11:18:51 +0200

    ---

--- neoforge/src/main/java/de/keksuccino/fancymenu/networking/bridge/BridgePacketPayloadNeoForge.java
+++ neoforge/src/main/java/de/keksuccino/fancymenu/networking/bridge/BridgePacketPayloadNeoForge.java
@@ -5,13 +5,14 @@ import de.keksuccino.fancymenu.networking.PacketPayloadBaseNeoForge;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class BridgePacketPayloadNeoForge extends PacketPayloadBaseNeoForge implements CustomPacketPayload {
 
-    public static final Type<BridgePacketPayloadNeoForge> TYPE = CustomPacketPayload.createType("fancymenu_packet_bridge");
+    public static final Type<BridgePacketPayloadNeoForge> TYPE = new CustomPacketPayload.Type<>(ResourceLocation.fromNamespaceAndPath("fancymenu", "fancymenu_bridge_packet"));
     public static final StreamCodec<FriendlyByteBuf, BridgePacketPayloadNeoForge> CODEC = CustomPacketPayload.codec(BridgePacketPayloadNeoForge::write, BridgePacketPayloadNeoForge::new);
 
     public String dataWithIdentifier;
