695283a52552ac1c1db1d40a01c90fe91ab66910
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-10-13 11:12:53 +0200

    v3.3.1

--- common/src/main/java/de/keksuccino/fancymenu/FancyMenu.java
+++ common/src/main/java/de/keksuccino/fancymenu/FancyMenu.java
@@ -29,7 +29,7 @@ public class FancyMenu {
 	private static final Logger LOGGER = LogManager.getLogger();
 
 	//TODO übernehmen
-	public static final String VERSION = "3.3.0";
+	public static final String VERSION = "3.3.1";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 	public static final String MOD_ID = "fancymenu";
 
