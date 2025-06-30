be802cb9f2163b09ddde072cbc3f3bcf42be8882
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-06-30 22:50:41 +0200

    v3.6.1

--- common/src/main/java/de/keksuccino/fancymenu/Test.java
+++ common/src/main/java/de/keksuccino/fancymenu/Test.java
@@ -3,18 +3,11 @@ package de.keksuccino.fancymenu;
 import de.keksuccino.fancymenu.events.screen.InitOrResizeScreenCompletedEvent;
 import de.keksuccino.fancymenu.util.event.acara.EventListener;
 import de.keksuccino.fancymenu.util.event.acara.EventPriority;
-import de.keksuccino.fancymenu.util.rendering.ui.widget.button.ExtendedButton;
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.Component;
 
 public class Test {
 
     @EventListener(priority = EventPriority.VERY_LOW)
     public void onRenderPost(InitOrResizeScreenCompletedEvent e) {
-
-        e.addRenderableWidget(new ExtendedButton(20, 20, 200, 20, "Open Video Player", button -> {
-            Minecraft.getInstance().setScreen(new McefExampleScreen(Component.empty()));
-        }));
 
     }
 
