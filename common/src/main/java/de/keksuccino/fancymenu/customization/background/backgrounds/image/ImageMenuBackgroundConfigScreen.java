911de0947314ac7206ab83b597db71502459c840
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-08-07 02:31:41 +0200

    v3.7.0 - 26

--- common/src/main/java/de/keksuccino/fancymenu/customization/background/backgrounds/image/ImageMenuBackgroundConfigScreen.java
+++ common/src/main/java/de/keksuccino/fancymenu/customization/background/backgrounds/image/ImageMenuBackgroundConfigScreen.java
@@ -1,13 +1,10 @@
 package de.keksuccino.fancymenu.customization.background.backgrounds.image;
 
 import de.keksuccino.fancymenu.util.LocalizationUtils;
-import de.keksuccino.fancymenu.util.SerializationUtils;
 import de.keksuccino.fancymenu.util.cycle.CommonCycles;
 import de.keksuccino.fancymenu.util.file.type.types.ImageFileType;
-import de.keksuccino.fancymenu.util.input.CharacterFilter;
 import de.keksuccino.fancymenu.util.rendering.ui.UIBase;
 import de.keksuccino.fancymenu.util.rendering.ui.screen.CellScreen;
-import de.keksuccino.fancymenu.util.rendering.ui.screen.TextInputScreen;
 import de.keksuccino.fancymenu.util.rendering.ui.screen.resource.ResourceChooserScreen;
 import de.keksuccino.fancymenu.util.rendering.ui.screen.texteditor.TextEditorScreen;
 import de.keksuccino.fancymenu.util.rendering.ui.tooltip.Tooltip;
@@ -16,7 +13,6 @@ import de.keksuccino.fancymenu.util.rendering.ui.widget.button.ExtendedButton;
 import de.keksuccino.fancymenu.util.resource.ResourceSupplier;
 import de.keksuccino.fancymenu.util.resource.resources.texture.ITexture;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
@@ -68,6 +64,10 @@ public class ImageMenuBackgroundConfigScreen extends CellScreen {
 
         this.addCellGroupEndSpacerCell();
 
+        this.addCycleButtonCell(CommonCycles.cycleEnabledDisabled("fancymenu.background.image.restart_animated_on_menu_load", this.background.restartAnimatedOnMenuLoad), true, (value, button) -> {
+            this.background.restartAnimatedOnMenuLoad = value.getAsBoolean();
+        });
+
         WidgetCell repeatButton = this.addCycleButtonCell(CommonCycles.cycleEnabledDisabled("fancymenu.background.image.configure.repeat", this.background.repeat), true, (value, button) -> {
             this.background.repeat = value.getAsBoolean();
         });
@@ -88,7 +88,6 @@ public class ImageMenuBackgroundConfigScreen extends CellScreen {
         if (parallaxButton.widget instanceof CycleButton<?> b) {
             b.setIsActiveSupplier(consumes -> !this.background.slideLeftRight);
         }
-
 
         this.addWidgetCell(new ExtendedButton(0, 0, 0, 20, Component.translatable("fancymenu.background.image.configure.parallax_intensity"), var1 -> {
             final Screen currentScreen = Minecraft.getInstance().screen;
