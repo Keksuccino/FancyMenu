9c8c403aadd238ead64bd69cf2b54e5a9271ae57
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-08-31 22:25:35 +0200

    v3.7.1

--- common/src/main/java/de/keksuccino/fancymenu/FancyMenu.java
+++ common/src/main/java/de/keksuccino/fancymenu/FancyMenu.java
@@ -26,7 +26,7 @@ public class FancyMenu {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	public static final String VERSION = "3.7.0";
+	public static final String VERSION = "3.7.1";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 	public static final String MOD_ID = "fancymenu";
 
--- fabric/src/main/java/de/keksuccino/fancymenu/mixin/mixins/fabric/client/MixinFabricMouseHandler.java
+++ fabric/src/main/java/de/keksuccino/fancymenu/mixin/mixins/fabric/client/MixinFabricMouseHandler.java
@@ -24,25 +24,6 @@ public class MixinFabricMouseHandler {
     @Unique private int cachedMouseButton_FancyMenu = -1;
 
     /**
-     * @reason This restores Minecraft's old UI component click logic to not only click the hovered component, but all of them. The old logic is only used for FancyMenu's components.
-     */
-    @WrapOperation(method = "onPress", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;mouseClicked(DDI)Z"))
-    private boolean wrap_Screen_mouseClicked_in_onPress_FancyMenu(Screen instance, double mouseX, double mouseY, int button, Operation<Boolean> original) {
-        for (GuiEventListener listener : instance.children()) {
-            if (listener instanceof FancyMenuUiComponent) {
-                if (listener.mouseClicked(mouseX, mouseY, button)) {
-                    instance.setFocused(listener);
-                    if (button == 0) {
-                        instance.setDragging(true);
-                    }
-                    return true;
-                }
-            }
-        }
-        return original.call(instance, mouseX, mouseY, button);
-    }
-
-    /**
      * @reason This restores Minecraft's old UI component scroll logic to not only scroll the hovered component, but all of them. The old logic is only used for FancyMenu's components.
      */
     @WrapOperation(method = "onScroll", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;mouseScrolled(DDDD)Z"))
@@ -75,6 +56,10 @@ public class MixinFabricMouseHandler {
             for (GuiEventListener listener : instance.children()) {
                 if (listener instanceof FancyMenuUiComponent) {
                     if (listener.mouseClicked(d, e, this.cachedMouseButton_FancyMenu)) {
+                        instance.setFocused(listener);
+                        if (this.cachedMouseButton_FancyMenu == 0) {
+                            instance.setDragging(true);
+                        }
                         info.cancel();
                         return;
                     }
@@ -96,6 +81,9 @@ public class MixinFabricMouseHandler {
             for (GuiEventListener listener : instance.children()) {
                 if (listener instanceof FancyMenuUiComponent) {
                     if (listener.mouseReleased(d, e, this.cachedMouseButton_FancyMenu)) {
+                        if ((this.cachedMouseButton_FancyMenu == 0) && instance.isDragging()) {
+                            instance.setDragging(false);
+                        }
                         info.cancel();
                         return;
                     }
