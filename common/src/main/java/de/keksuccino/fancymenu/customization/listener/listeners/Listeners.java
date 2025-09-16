b08154be25450a6b4ed0da9d53751b734f6ab090
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-09-16 23:21:31 +0200

    v3.8.0 - 49

--- common/src/main/java/de/keksuccino/fancymenu/customization/listener/listeners/Listeners.java
+++ common/src/main/java/de/keksuccino/fancymenu/customization/listener/listeners/Listeners.java
@@ -7,12 +7,14 @@ public class Listeners {
     public static final OnCharTypedListener ON_CHAR_TYPED = new OnCharTypedListener();
     public static final OnVariableUpdatedListener ON_VARIABLE_UPDATED = new OnVariableUpdatedListener();
     public static final OnFileDownloadedListener ON_FILE_DOWNLOADED = new OnFileDownloadedListener();
+    public static final OnLookingAtBlockListener ON_LOOKING_AT_BLOCK = new OnLookingAtBlockListener();
 
     public static void registerAll() {
 
         ListenerRegistry.register(ON_CHAR_TYPED);
         ListenerRegistry.register(ON_VARIABLE_UPDATED);
         ListenerRegistry.register(ON_FILE_DOWNLOADED);
+        ListenerRegistry.register(ON_LOOKING_AT_BLOCK);
 
     }
 
--- common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinGameRenderer.java
+++ common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinGameRenderer.java
@@ -1,9 +1,18 @@
 package de.keksuccino.fancymenu.mixin.mixins.common.client;
 
 import de.keksuccino.fancymenu.customization.ScreenCustomization;
+import de.keksuccino.fancymenu.customization.listener.listeners.Listeners;
 import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,10 +20,40 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 public class MixinGameRenderer {
 
+    @Shadow @Final Minecraft minecraft;
+
     @Inject(method = "render", at = @At("HEAD"))
     private void before_render_FancyMenu(DeltaTracker $$0, boolean $$1, CallbackInfo info) {
-
         ScreenCustomization.onPreGameRenderTick();
+    }
+
+    @Inject(method = "pick(F)V", at = @At("TAIL"))
+    private void fancymenu$onPick(float partialTicks, CallbackInfo info) {
+
+        if (this.minecraft == null) {
+            return;
+        }
+
+        HitResult hitResult = this.minecraft.hitResult;
+        if (!(hitResult instanceof BlockHitResult blockHitResult)) {
+            Listeners.ON_LOOKING_AT_BLOCK.onStopLooking();
+            return;
+        }
+
+        if (!(this.minecraft.level instanceof ClientLevel clientLevel)) {
+            Listeners.ON_LOOKING_AT_BLOCK.onStopLooking();
+            return;
+        }
+
+        Entity cameraEntity = this.minecraft.getCameraEntity();
+        if (cameraEntity == null) {
+            Listeners.ON_LOOKING_AT_BLOCK.onStopLooking();
+            return;
+        }
+
+        Vec3 eyePosition = cameraEntity.getEyePosition(partialTicks);
+        double distance = blockHitResult.getLocation().distanceTo(eyePosition);
+        Listeners.ON_LOOKING_AT_BLOCK.onLookAtBlock(clientLevel, blockHitResult, distance);
 
     }
 
