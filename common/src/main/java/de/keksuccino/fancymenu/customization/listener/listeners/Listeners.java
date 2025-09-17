090f911d318ef1bf3db0eb166cd55cac2ab36b2b
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-09-17 23:08:04 +0200

    v3.8.0 - 59

--- common/src/main/java/de/keksuccino/fancymenu/customization/listener/listeners/Listeners.java
+++ common/src/main/java/de/keksuccino/fancymenu/customization/listener/listeners/Listeners.java
@@ -6,6 +6,7 @@ public class Listeners {
 
     public static final OnCharTypedListener ON_CHAR_TYPED = new OnCharTypedListener();
     public static final OnMouseMovedListener ON_MOUSE_MOVED = new OnMouseMovedListener();
+    public static final OnMouseScrolledListener ON_MOUSE_SCROLLED = new OnMouseScrolledListener();
     public static final OnVariableUpdatedListener ON_VARIABLE_UPDATED = new OnVariableUpdatedListener();
     public static final OnFileDownloadedListener ON_FILE_DOWNLOADED = new OnFileDownloadedListener();
     public static final OnLookingAtBlockListener ON_LOOKING_AT_BLOCK = new OnLookingAtBlockListener();
@@ -20,6 +21,7 @@ public class Listeners {
 
         ListenerRegistry.register(ON_CHAR_TYPED);
         ListenerRegistry.register(ON_MOUSE_MOVED);
+        ListenerRegistry.register(ON_MOUSE_SCROLLED);
         ListenerRegistry.register(ON_VARIABLE_UPDATED);
         ListenerRegistry.register(ON_FILE_DOWNLOADED);
         ListenerRegistry.register(ON_LOOKING_AT_BLOCK);
