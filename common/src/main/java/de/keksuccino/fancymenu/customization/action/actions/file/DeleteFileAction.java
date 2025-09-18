94c2cf13bac725f90cc2699b0ff8eae293bd3393
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-09-19 01:26:37 +0200

    v3.8.0 - 71

--- common/src/main/java/de/keksuccino/fancymenu/customization/action/actions/file/DeleteFileAction.java
+++ common/src/main/java/de/keksuccino/fancymenu/customization/action/actions/file/DeleteFileAction.java
@@ -10,6 +10,13 @@ import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class DeleteFileAction extends Action {
 
@@ -27,15 +34,24 @@ public class DeleteFileAction extends Action {
     @Override
     public void execute(@Nullable String value) {
         try {
-            if (value != null) {
-                // We only allow the default .minecraft directory and the instance's actual game directory for safety reasons
-                value = DotMinecraftUtils.resolveMinecraftPath(value);
-                if (!DotMinecraftUtils.isInsideMinecraftDirectory(value)) {
-                    value = GameDirectoryUtils.getAbsoluteGameDirectoryPath(value);
+            if ((value != null) && !value.isEmpty()) {
+                boolean wildcardTarget = isWildcardPath(value);
+                String resolvedPath = resolveActionPath(value, wildcardTarget);
+                File targetFile = new File(resolvedPath);
+                if (!targetFile.exists()) {
+                    throw new FileNotFoundException("Target not found! Can't delete: " + (wildcardTarget ? value : resolvedPath));
                 }
-                File f = new File(value);
-                if (f.isFile()) {
-                    f.delete();
+                if (wildcardTarget) {
+                    if (!targetFile.isDirectory()) {
+                        throw new FileNotFoundException("Target directory not found! Can't delete: " + value);
+                    }
+                    deleteWildcardFiles(targetFile);
+                    return;
+                }
+                if (targetFile.isDirectory()) {
+                    deleteDirectoryRecursively(targetFile);
+                } else if (targetFile.isFile()) {
+                    Files.delete(targetFile.toPath());
                 }
             }
         } catch (Exception ex) {
@@ -60,7 +76,61 @@ public class DeleteFileAction extends Action {
 
     @Override
     public String getValueExample() {
-        return "/config/some_mod_folder/file.txt";
+        return "/config/some_mod_folder/*";
+    }
+
+    private void deleteDirectoryRecursively(@NotNull File directory) throws IOException {
+        Path directoryPath = directory.toPath();
+        Files.walkFileTree(directoryPath, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                if (exc != null) {
+                    throw exc;
+                }
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    private void deleteWildcardFiles(@NotNull File directory) throws IOException {
+        File[] filesToDelete = directory.listFiles(File::isFile);
+        if (filesToDelete == null) {
+            throw new IOException("Failed to list files in target directory: " + directory.getAbsolutePath());
+        }
+        for (File file : filesToDelete) {
+            Files.delete(file.toPath());
+        }
+    }
+
+    private @NotNull String resolveActionPath(@NotNull String path, boolean wildcard) {
+        String processedPath = wildcard ? stripTrailingWildcard(path) : path;
+        String resolvedPath = DotMinecraftUtils.resolveMinecraftPath(processedPath);
+        if (!DotMinecraftUtils.isInsideMinecraftDirectory(resolvedPath)) {
+            resolvedPath = GameDirectoryUtils.getAbsoluteGameDirectoryPath(resolvedPath);
+        }
+        return resolvedPath;
+    }
+
+    private @NotNull String stripTrailingWildcard(@NotNull String path) {
+        if (path.length() <= 1) {
+            throw new IllegalArgumentException("Wildcard path requires a directory before '*': " + path);
+        }
+        String withoutWildcard = path.substring(0, path.length() - 1);
+        if (withoutWildcard.isEmpty()) {
+            throw new IllegalArgumentException("Wildcard path requires a directory before '*': " + path);
+        }
+        return withoutWildcard;
+    }
+
+    private boolean isWildcardPath(@Nullable String path) {
+        return (path != null) && path.endsWith("*");
     }
 
 }
