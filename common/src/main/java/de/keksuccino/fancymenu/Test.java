6b0e0de6593bf755502dbb04c0695feda054890b
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-09-20 00:51:18 +0200

    v3.8.0 - 77

--- common/src/main/java/de/keksuccino/fancymenu/Test.java
+++ common/src/main/java/de/keksuccino/fancymenu/Test.java
@@ -1,8 +1,12 @@
 package de.keksuccino.fancymenu;
 
 import de.keksuccino.fancymenu.events.screen.InitOrResizeScreenCompletedEvent;
+import de.keksuccino.fancymenu.events.screen.RenderScreenEvent;
 import de.keksuccino.fancymenu.util.event.acara.EventListener;
 import de.keksuccino.fancymenu.util.event.acara.EventPriority;
+import de.keksuccino.fancymenu.util.rendering.GuiBlurUtils;
+import de.keksuccino.fancymenu.util.rendering.ui.UIBase;
+import net.minecraft.client.Minecraft;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,7 +15,11 @@ public class Test {
     private static final Logger LOGGER = LogManager.getLogger();
 
     @EventListener(priority = EventPriority.VERY_LOW)
-    public void onRenderPost(InitOrResizeScreenCompletedEvent e) {
+    public void onRenderPost(RenderScreenEvent.Post e) {
+
+        GuiBlurUtils.applyBlurArea(e.getGraphics(), 20, 20, 200, 300, -1, 0.5F, true);
+
+        e.getGraphics().drawString(Minecraft.getInstance().font, "This is a test.", 50, 50, UIBase.getUIColorTheme().warning_text_color.getColorInt());
 
     }
 
