74ecf27ed5ff6b479d709944a64c9031474e771c
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-09-22 23:07:10 +0200

    v3.8.0 - 95

--- common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinLevelRenderer.java
+++ common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinLevelRenderer.java
@@ -6,15 +6,21 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import de.keksuccino.fancymenu.customization.listener.listeners.Listeners;
 import net.minecraft.client.Camera;
 import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -34,13 +40,45 @@ public class MixinLevelRenderer {
         double interpolatedZ = Mth.lerp(partialTicks, entity.zo, entity.getZ());
         Vec3 entityPosition = new Vec3(interpolatedX, interpolatedY, interpolatedZ);
         Vec3 cameraPosition = new Vec3(cameraX, cameraY, cameraZ);
-        double distance = entityPosition.distanceTo(cameraPosition);
-        Listeners.ON_ENTITY_IN_SIGHT.onEntityVisible(entity, distance);
+        if (isEntityVisibleForListener_FancyMenu(entity, cameraPosition, entityPosition)) {
+            double distance = entityPosition.distanceTo(cameraPosition);
+            Listeners.ON_ENTITY_IN_SIGHT.onEntityVisible(entity, distance);
+        }
         original.call(levelRenderer, entity, cameraX, cameraY, cameraZ, partialTicks, poseStack, bufferSource);
     }
 
     @Inject(method = "renderLevel(Lnet/minecraft/client/DeltaTracker;ZLnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/GameRenderer;Lnet/minecraft/client/renderer/LightTexture;Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;)V", at = @At("TAIL"))
     private void after_renderLevel_FancyMenu(DeltaTracker deltaTracker, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f projectionMatrix, Matrix4f frustumMatrix, CallbackInfo info) {
         Listeners.ON_ENTITY_IN_SIGHT.onRenderFrameEnd();
+    }
+
+    @Unique
+    private static boolean isEntityVisibleForListener_FancyMenu(Entity entity, Vec3 cameraPosition, Vec3 entityPosition) {
+        Minecraft minecraft = Minecraft.getInstance();
+        Entity cameraEntity = minecraft.getCameraEntity();
+        if (cameraEntity == null) {
+            return true;
+        }
+        if (entity == cameraEntity) {
+            return false;
+        }
+
+        double distanceToEntity = entityPosition.distanceTo(cameraPosition);
+        if (distanceToEntity <= 1.0E-8) {
+            return false;
+        }
+
+        BlockHitResult hitResult = entity.level().clip(new ClipContext(cameraPosition, entityPosition, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, cameraEntity));
+        if (hitResult.getType() != HitResult.Type.BLOCK) {
+            return true;
+        }
+
+        double blockDistance = hitResult.getLocation().distanceTo(cameraPosition);
+        if (blockDistance >= distanceToEntity - 1.0E-4) {
+            return true;
+        }
+
+        BlockState blockState = entity.level().getBlockState(hitResult.getBlockPos());
+        return !blockState.canOcclude();
     }
 }
