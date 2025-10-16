e11d32105e20b94798d7b2476837bdf271774574
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-10-16 07:43:37 +0200

    v3.7.2

--- forge/src/main/java/de/keksuccino/fancymenu/networking/bridge/BridgePacketMessageForge.java
+++
@@ -1,9 +0,0 @@
-package de.keksuccino.fancymenu.networking.bridge;
-
-import de.keksuccino.fancymenu.networking.PacketMessageBaseForge;
-
-public class BridgePacketMessageForge extends PacketMessageBaseForge {
-
-    public String dataWithIdentifier;
-
-}
