a27da7f262da1f271272b0a692f72c9dc72c581e
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-11-24 06:34:31 +0100

    v3.8.1

--- common/src/main/java/de/keksuccino/fancymenu/FancyMenu.java
+++ common/src/main/java/de/keksuccino/fancymenu/FancyMenu.java
@@ -27,7 +27,7 @@ public class FancyMenu {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	public static final String VERSION = "3.8.0";
+	public static final String VERSION = "3.8.1";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 	public static final String MOD_ID = "fancymenu";
 
