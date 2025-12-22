cfce5d565ea55d3e76d62a82119f47fabc035a46
Keksuccino <no@mail.net>
2025-12-22 11:03:55 +0100

    v3.9.0

--- common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinCreativeModeInventoryScreen.java
+++ common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinCreativeModeInventoryScreen.java
@@ -1,21 +1,39 @@
 package de.keksuccino.fancymenu.mixin.mixins.common.client;
 
 import de.keksuccino.fancymenu.customization.ScreenCustomization;
+import de.keksuccino.fancymenu.customization.overlay.CustomizationOverlay;
+import de.keksuccino.fancymenu.customization.overlay.CustomizationOverlayMenuBar;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CreativeModeInventoryScreen.class)
 public abstract class MixinCreativeModeInventoryScreen extends Screen {
 
     @Unique private boolean resized_FancyMenu = false;
 
+    // Dummy constructor
     private MixinCreativeModeInventoryScreen() {
-        super(null);
+        super(Component.empty());
+    }
+
+    /**
+     * @reason Fixes FancyMenu's menu not counting as top-level widget, so creative mode tabs get clicked below it
+     */
+    @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
+    private void head_mouseClicked_FancyMenu(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> info) {
+        CustomizationOverlayMenuBar bar = CustomizationOverlay.getCurrentMenuBarInstance();
+        if (bar != null) {
+            if (bar.isExpanded() && bar.isUserNavigatingInMenuBar()) {
+                info.setReturnValue(bar.mouseClicked(mouseX, mouseY, button));
+            }
+        }
     }
 
     /**
