837be6647f2727a42ec15020874cbb86b8c15452
Keksuccino <no@mail.net>
2025-12-24 09:17:52 +0100

    Porting to MC 1.21.11

--- common/src/main/java/de/keksuccino/fancymenu/FancyMenu.java
+++ common/src/main/java/de/keksuccino/fancymenu/FancyMenu.java
@@ -26,7 +26,7 @@ public class FancyMenu {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	public static final String VERSION = "3.8.2";
+	public static final String VERSION = "3.8.3";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 	public static final String MOD_ID = "fancymenu";
 
