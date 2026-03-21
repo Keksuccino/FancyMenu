61b6dbbc2a7bad2f16009120667e9d160c3b2d9a
Keksuccino <no@mail.net>
2026-03-21 15:54:56 +0100

    Fix recursive hover requirement checks

--- common/src/main/java/de/keksuccino/fancymenu/customization/requirement/requirements/gui/HoverRequirementUtils.java
+++ common/src/main/java/de/keksuccino/fancymenu/customization/requirement/requirements/gui/HoverRequirementUtils.java
@@ -4,7 +4,12 @@ import de.keksuccino.fancymenu.customization.element.AbstractElement;
 import de.keksuccino.fancymenu.customization.element.HideableElement;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
+import java.util.Set;
+
 final class HoverRequirementUtils {
+
+    private static final ThreadLocal<Set<String>> ACTIVE_HOVER_CHECKS = ThreadLocal.withInitial(HashSet::new);
 
     private HoverRequirementUtils() {
     }
@@ -13,7 +18,7 @@ final class HoverRequirementUtils {
         if (element instanceof HideableElement hideable && hideable.isHidden()) {
             return false;
         }
-        if (!element.shouldRender()) {
+        if (!shouldRenderForHoverCheck(element)) {
             return false;
         }
 
@@ -26,6 +31,23 @@ final class HoverRequirementUtils {
         int elementX = element.getAbsoluteX();
         int elementY = element.getAbsoluteY();
         return (mouseX >= elementX) && (mouseX <= (elementX + elementWidth)) && (mouseY >= elementY) && (mouseY <= (elementY + elementHeight));
+    }
+
+    private static boolean shouldRenderForHoverCheck(@NotNull AbstractElement element) {
+        String instanceIdentifier = element.getInstanceIdentifier();
+        Set<String> activeHoverChecks = ACTIVE_HOVER_CHECKS.get();
+        if (!activeHoverChecks.add(instanceIdentifier)) {
+            // Prevent self-referential hover requirements from recursing through shouldRender().
+            return false;
+        }
+        try {
+            return element.shouldRender();
+        } finally {
+            activeHoverChecks.remove(instanceIdentifier);
+            if (activeHoverChecks.isEmpty()) {
+                ACTIVE_HOVER_CHECKS.remove();
+            }
+        }
     }
 
 }
