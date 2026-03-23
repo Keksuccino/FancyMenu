cc6751c9da46882a602270764b47d59bac2729da
Keksuccino <no@mail.net>
2026-03-23 15:12:10 +0100

    Tighten AFMA PNG payload compression

--- common/src/main/java/de/keksuccino/fancymenu/util/resource/resources/texture/afma/creator/AfmaPixelFrame.java
+++ common/src/main/java/de/keksuccino/fancymenu/util/resource/resources/texture/afma/creator/AfmaPixelFrame.java
@@ -2,10 +2,15 @@ package de.keksuccino.fancymenu.util.resource.resources.texture.afma.creator;
 
 import org.jetbrains.annotations.NotNull;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Arrays;
 import java.util.Objects;
 
@@ -66,12 +71,48 @@ public class AfmaPixelFrame implements AutoCloseable {
     public @NotNull byte[] asByteArray(boolean includeAlpha) throws IOException {
         BufferedImage image = new BufferedImage(this.width, this.height, includeAlpha ? BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_INT_RGB);
         image.setRGB(0, 0, this.width, this.height, this.pixels, 0, this.width);
+        return writeCompressedPng(image);
+    }
 
-        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-            if (!ImageIO.write(image, "png", out)) {
-                throw new IOException("Failed to encode AFMA PNG payload");
+    protected static @NotNull byte[] writeCompressedPng(@NotNull BufferedImage image) throws IOException {
+        Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("png");
+        if (!writers.hasNext()) {
+            try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                if (!ImageIO.write(image, "png", out)) {
+                    throw new IOException("Failed to encode AFMA PNG payload");
+                }
+                return out.toByteArray();
             }
+        }
+
+        ImageWriter writer = writers.next();
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+             ImageOutputStream imageOut = ImageIO.createImageOutputStream(out)) {
+            if (imageOut == null) {
+                throw new IOException("Failed to create AFMA PNG image output stream");
+            }
+
+            writer.setOutput(imageOut);
+            ImageWriteParam writeParam = writer.getDefaultWriteParam();
+            if (writeParam.canWriteCompressed()) {
+                writeParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+                String[] compressionTypes = writeParam.getCompressionTypes();
+                if ((compressionTypes != null) && (compressionTypes.length > 0)) {
+                    writeParam.setCompressionType(compressionTypes[0]);
+                }
+                // Prefer maximum deflate compression because AFMA prioritizes final archive size.
+                writeParam.setCompressionQuality(0.0F);
+            }
+
+            writer.write(null, new IIOImage(image, null, null), writeParam);
+            writer.dispose();
             return out.toByteArray();
+        } catch (Exception ex) {
+            writer.dispose();
+            if (ex instanceof IOException ioException) {
+                throw ioException;
+            }
+            throw new IOException("Failed to encode AFMA PNG payload", ex);
         }
     }
 
