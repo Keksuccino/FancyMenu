6497a23000b9cb842a668097e782dcfa73b85d38
Keksuccino <no@mail.net>
2026-04-06 02:37:44 +0200

    Port to MC 26.1.1

--- common/src/main/java/de/keksuccino/fancymenu/customization/element/elements/playerentity/PlayerEntityElement.java
+++ common/src/main/java/de/keksuccino/fancymenu/customization/element/elements/playerentity/PlayerEntityElement.java
@@ -5,9 +5,10 @@ import de.keksuccino.fancymenu.customization.element.ElementBuilder;
 import de.keksuccino.fancymenu.customization.element.elements.playerentity.textures.CapeResourceSupplier;
 import de.keksuccino.fancymenu.customization.element.elements.playerentity.textures.SkinResourceSupplier;
 import de.keksuccino.fancymenu.customization.placeholder.PlaceholderParser;
-import de.keksuccino.fancymenu.util.ItemStackUtils;
 import de.keksuccino.fancymenu.util.SerializationUtils;
 import de.keksuccino.fancymenu.util.enums.LocalizedCycleEnum;
+import de.keksuccino.fancymenu.util.rendering.DrawableColor;
+import de.keksuccino.fancymenu.util.rendering.entity.FancyEntityRendererUtils;
 import de.keksuccino.fancymenu.util.rendering.entity.WrappedFancyPlayerWidget;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
@@ -27,10 +28,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import java.awt.*;
 
 public class PlayerEntityElement extends AbstractElement {
-    
+
     private static final Logger LOGGER = LogManager.getLogger();
+    private static final DrawableColor MISSING_FER_COLOR = DrawableColor.of(Color.RED);
+
     public volatile boolean copyClientPlayer = false;
     @NotNull
     public volatile String playerName = "Steve";
@@ -109,7 +113,9 @@ public class PlayerEntityElement extends AbstractElement {
     @Override
     public void afterConstruction() {
         super.afterConstruction();
-        this.widget = WrappedFancyPlayerWidget.build(this.getAbsoluteX(), this.getAbsoluteY(), this.getAbsoluteWidth(), this.getAbsoluteHeight());
+        if (FancyEntityRendererUtils.isFerLoaded()) {
+            this.widget = WrappedFancyPlayerWidget.build(this.getAbsoluteX(), this.getAbsoluteY(), this.getAbsoluteWidth(), this.getAbsoluteHeight());
+        }
     }
 
     @Override
@@ -117,7 +123,15 @@ public class PlayerEntityElement extends AbstractElement {
 
         if (this.shouldRender()) {
 
-            if (this.widget != null) {
+            if (this.widget == null) {
+
+                graphics.fill(this.getAbsoluteX(), this.getAbsoluteY(), this.getAbsoluteX() + this.getAbsoluteWidth(), this.getAbsoluteY() + this.getAbsoluteHeight(), MISSING_FER_COLOR.getColorInt());
+                int xCenter = this.getAbsoluteX() + (this.getAbsoluteWidth() / 2);
+                int yCenter = this.getAbsoluteY() + (this.getAbsoluteHeight() / 2);
+                graphics.centeredText(Minecraft.getInstance().font, "§lFER (FANCY ENTITY RENDERER) IS NOT INSTALLED!", xCenter, yCenter, -1);
+                graphics.centeredText(Minecraft.getInstance().font, "§lPLEASE DOWNLOAD FROM CURSEFORGE OR MODRINTH!", xCenter, yCenter + Minecraft.getInstance().font.lineHeight + 2, -1);
+
+            } else {
 
                 this.widget.setX(this.getAbsoluteX());
                 this.widget.setY(this.getAbsoluteY());
@@ -135,8 +149,11 @@ public class PlayerEntityElement extends AbstractElement {
                 this.updateEntityProperties();
 
                 this.widget.extractRenderState(graphics, mouseX, mouseY, partial);
+
             }
+
         }
+
     }
 
     protected void updateEntityProperties() {
@@ -360,7 +377,7 @@ public class PlayerEntityElement extends AbstractElement {
         public String itemKey;
         public boolean enchanted;
         @NotNull
-        public ItemStack cachedStack = ItemStack.EMPTY;
+        public ItemStack cachedStack = new ItemStack(Items.AIR);
         @Nullable
         protected String lastFinalKey = null;
         protected boolean lastEnchanted = false;
@@ -387,7 +404,7 @@ public class PlayerEntityElement extends AbstractElement {
                     this.lastEnchanted = enchanted;
 
                     Item item = BuiltInRegistries.ITEM.getValue(Identifier.parse(keyFinal));
-                    this.cachedStack = ItemStackUtils.createDisplayStack(item);
+                    this.cachedStack = new ItemStack(item);
                     this.cachedStack.set(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, this.enchanted);
 
                 }
--- common/src/main/java/de/keksuccino/fancymenu/util/rendering/entity/WrappedFancyPlayerWidget.java
+++ common/src/main/java/de/keksuccino/fancymenu/util/rendering/entity/WrappedFancyPlayerWidget.java
@@ -1,9 +1,9 @@
 package de.keksuccino.fancymenu.util.rendering.entity;
 
-import de.keksuccino.fancymenu.util.ItemStackUtils;
 import de.keksuccino.fancymenu.util.rendering.ui.widget.NavigatableWidget;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
+import it.crystalnest.fancy_entity_renderer.api.Rotation;
+import it.crystalnest.fancy_entity_renderer.api.entity.RenderMode;
+import it.crystalnest.fancy_entity_renderer.api.entity.player.FancyPlayerWidget;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
@@ -15,43 +15,16 @@ import net.minecraft.world.entity.animal.parrot.Parrot;
 import net.minecraft.world.entity.player.PlayerSkin;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
 import java.util.UUID;
 
-@SuppressWarnings("unused")
+@SuppressWarnings("all")
 public class WrappedFancyPlayerWidget extends AbstractWidget implements NavigatableWidget {
 
-    private static final int BACKGROUND_COLOR = 0xAA101318;
-    private static final int BORDER_COLOR = 0xFF3D4652;
-    private static final int HEADER_COLOR = 0xFF7AA2FF;
-
-    private boolean focusable;
-    private boolean navigatable;
     @NotNull
-    private String name = "Steve";
-    private boolean showName = true;
-    private boolean slim;
-    private boolean baby;
-    private boolean headFollowsMouse = true;
-    private boolean bodyFollowsMouse = true;
-    @NotNull
-    private Pose pose = Pose.STANDING;
-    @Nullable
-    private PlayerSkin skin;
-    @Nullable
-    private ItemStack leftHandItem;
-    @Nullable
-    private ItemStack rightHandItem;
-    @Nullable
-    private ItemStack headWearable;
-    @Nullable
-    private ItemStack chestWearable;
-    @Nullable
-    private ItemStack legsWearable;
-    @Nullable
-    private ItemStack feetWearable;
+    protected final FancyPlayerWidget wrapped;
 
     @NotNull
     public static WrappedFancyPlayerWidget build(int x, int y, int width, int height) {
@@ -60,296 +33,310 @@ public class WrappedFancyPlayerWidget extends AbstractWidget implements Navigata
 
     protected WrappedFancyPlayerWidget(int x, int y, int width, int height) {
         super(x, y, width, height, Component.empty());
+        this.wrapped = new FancyPlayerWidget(x, y, width, height);
     }
 
     @Override
     protected void extractWidgetRenderState(@NotNull GuiGraphicsExtractor graphics, int mouseX, int mouseY, float partial) {
-        Font font = Minecraft.getInstance().font;
-        int x = getX();
-        int y = getY();
-        int width = getWidth();
-        int height = getHeight();
-
-        graphics.fill(x, y, x + width, y + height, BACKGROUND_COLOR);
-        graphics.fill(x, y, x + width, y + 1, BORDER_COLOR);
-        graphics.fill(x, y + height - 1, x + width, y + height, BORDER_COLOR);
-        graphics.fill(x, y, x + 1, y + height, BORDER_COLOR);
-        graphics.fill(x + width - 1, y, x + width, y + height, BORDER_COLOR);
-
-        int titleY = y + 6;
-        int centerX = x + (width / 2);
-        graphics.centeredText(font, "Player Preview", centerX, titleY, HEADER_COLOR);
-
-        int infoY = titleY + font.lineHeight + 6;
-        if (this.showName) {
-            graphics.centeredText(font, this.name, centerX, infoY, 0xFFFFFFFF);
-            infoY += font.lineHeight + 4;
-        }
-
-        graphics.centeredText(font, "Pose: " + this.pose.name().toLowerCase(), centerX, infoY, 0xFFCDD6E0);
-        infoY += font.lineHeight + 3;
-
-        String behaviorLine = (this.headFollowsMouse ? "Head tracks cursor" : "Head fixed")
-                + " | "
-                + (this.bodyFollowsMouse ? "Body tracks cursor" : "Body fixed");
-        graphics.centeredText(font, behaviorLine, centerX, infoY, 0xFF9EA9B8);
-        infoY += font.lineHeight + 3;
-
-        String bodyLine = (this.slim ? "Slim" : "Wide")
-                + (this.baby ? " | Baby" : "")
-                + ((this.skin != null) ? " | Custom skin" : "");
-        graphics.centeredText(font, bodyLine, centerX, infoY, 0xFF9EA9B8);
-
-        int rowY = y + height - 24;
-        int slotY = rowY + 4;
-        int[] slotXs = {
-                x + 6,
-                x + 26,
-                x + 46,
-                x + width - 62,
-                x + width - 42,
-                x + width - 22
-        };
-        ItemStack[] stacks = {
-                this.leftHandItem,
-                this.rightHandItem,
-                this.headWearable,
-                this.chestWearable,
-                this.legsWearable,
-                this.feetWearable
-        };
-
-        for (int i = 0; i < slotXs.length; i++) {
-            int slotX = slotXs[i];
-            graphics.fill(slotX - 1, rowY - 1, slotX + 17, rowY + 17, BORDER_COLOR);
-            graphics.fill(slotX, rowY, slotX + 16, rowY + 16, 0x661A2029);
-            if ((stacks[i] != null) && !stacks[i].isEmpty()) {
-                graphics.pose().pushMatrix();
-                graphics.pose().translate(slotX, slotY);
-                graphics.item(stacks[i], 0, 0);
-                graphics.pose().popMatrix();
-            }
-        }
+        wrapped.extractRenderState(graphics, mouseX, mouseY, partial);
     }
 
     @Override
     protected void updateWidgetNarration(@NotNull NarrationElementOutput output) {
+        wrapped.updateNarration(output);
+    }
+
+    @Override
+    public void setX(int x) {
+        super.setX(x);
+        wrapped.setX(x);
+    }
+
+    @Override
+    public void setY(int y) {
+        super.setY(y);
+        wrapped.setY(y);
+    }
+
+    @Override
+    public void setWidth(int width) {
+        super.setWidth(width);
+        wrapped.setWidth(width);
+    }
+
+    @Override
+    public void setHeight(int height) {
+        super.setHeight(height);
+        wrapped.setHeight(height);
+    }
+
+    @Override
+    public void setSize(int width, int height) {
+        super.setSize(width, height);
+        wrapped.setSize(width, height);
     }
 
     public WrappedFancyPlayerWidget setBodyFollowsMouse(boolean followsMouse) {
-        this.bodyFollowsMouse = followsMouse;
+        wrapped.setBodyFollowsMouse(followsMouse);
         return this;
     }
 
     public WrappedFancyPlayerWidget setHeadFollowsMouse(boolean followsMouse) {
-        this.headFollowsMouse = followsMouse;
+        wrapped.setHeadFollowsMouse(followsMouse);
+        return this;
+    }
+
+    public WrappedFancyPlayerWidget setHeadRotation(Rotation rotation) {
+        wrapped.setHeadRotation(rotation);
         return this;
     }
 
     public WrappedFancyPlayerWidget setHeadRotation(float x, float y, float z) {
+        wrapped.setHeadRotation(x, y, z);
+        return this;
+    }
+
+    public WrappedFancyPlayerWidget setBodyRotation(Rotation rotation) {
+        wrapped.setBodyRotation(rotation);
         return this;
     }
 
     public WrappedFancyPlayerWidget setBodyRotation(float x, float y, float z) {
+        wrapped.setBodyRotation(x, y, z);
+        return this;
+    }
+
+    public WrappedFancyPlayerWidget setLeftArmRotation(Rotation rotation) {
+        wrapped.setLeftArmRotation(rotation);
         return this;
     }
 
     public WrappedFancyPlayerWidget setLeftArmRotation(float x, float y, float z) {
+        wrapped.setLeftArmRotation(x, y, z);
+        return this;
+    }
+
+    public WrappedFancyPlayerWidget setRightArmRotation(Rotation rotation) {
+        wrapped.setRightArmRotation(rotation);
         return this;
     }
 
     public WrappedFancyPlayerWidget setRightArmRotation(float x, float y, float z) {
+        wrapped.setRightArmRotation(x, y, z);
+        return this;
+    }
+
+    public WrappedFancyPlayerWidget setLeftLegRotation(Rotation rotation) {
+        wrapped.setLeftLegRotation(rotation);
         return this;
     }
 
     public WrappedFancyPlayerWidget setLeftLegRotation(float x, float y, float z) {
+        wrapped.setLeftLegRotation(x, y, z);
+        return this;
+    }
+
+    public WrappedFancyPlayerWidget setRightLegRotation(Rotation rotation) {
+        wrapped.setRightLegRotation(rotation);
         return this;
     }
 
     public WrappedFancyPlayerWidget setRightLegRotation(float x, float y, float z) {
+        wrapped.setRightLegRotation(x, y, z);
         return this;
     }
 
     public WrappedFancyPlayerWidget setSlim(boolean isSlim) {
-        this.slim = isSlim;
+        wrapped.setSlim(isSlim);
         return this;
     }
 
     public WrappedFancyPlayerWidget setSkin(@Nullable PlayerSkin skin) {
-        this.skin = skin;
+        wrapped.setSkin(skin);
         return this;
     }
 
     public WrappedFancyPlayerWidget copyLocalPlayer() {
+        wrapped.copyLocalPlayer();
         return this;
     }
 
     public WrappedFancyPlayerWidget setName(String name) {
-        this.name = (name == null || name.isBlank()) ? "Steve" : name;
+        wrapped.setName(name);
         return this;
     }
 
     public WrappedFancyPlayerWidget setShowName(boolean showName) {
-        this.showName = showName;
+        wrapped.setShowName(showName);
         return this;
     }
 
     public WrappedFancyPlayerWidget setUpsideDown(boolean isUpsideDown) {
+        wrapped.setUpsideDown(isUpsideDown);
         return this;
     }
 
     public WrappedFancyPlayerWidget setPose(@NotNull Pose pose) {
-        this.pose = pose;
+        wrapped.setPose(pose);
         return this;
     }
 
+    public WrappedFancyPlayerWidget setRenderMode(@NotNull RenderMode renderMode) {
+        wrapped.setRenderMode(renderMode);
+        return this;
+    }
+
+    @ApiStatus.Experimental
     public WrappedFancyPlayerWidget setGlowing(int glowColor) {
+        wrapped.setGlowing(glowColor);
         return this;
     }
 
+    @ApiStatus.Experimental
     public WrappedFancyPlayerWidget setMoving(boolean isMoving) {
+        wrapped.setMoving(isMoving);
         return this;
     }
 
     public WrappedFancyPlayerWidget setOnFire(boolean onFire) {
+        wrapped.setOnFire(onFire);
         return this;
     }
 
     public WrappedFancyPlayerWidget setOnFire(boolean onFire, Identifier fireType) {
+        wrapped.setOnFire(onFire, fireType);
         return this;
     }
 
     public WrappedFancyPlayerWidget setBaby(boolean isBaby) {
-        this.baby = isBaby;
+        wrapped.setBaby(isBaby);
         return this;
     }
 
     public WrappedFancyPlayerWidget setParrots(@Nullable Parrot.Variant leftParrot, @Nullable Parrot.Variant rightParrot) {
+        wrapped.setParrots(leftParrot, rightParrot);
         return this;
     }
 
     public WrappedFancyPlayerWidget setBodyMovement(boolean shouldMove) {
+        wrapped.setMoving(shouldMove);
         return this;
     }
 
     public WrappedFancyPlayerWidget setRightHandItem(@Nullable Item item) {
-        this.rightHandItem = item == null ? null : ItemStackUtils.createDisplayStack(item);
+        wrapped.setRightHandItem(item);
         return this;
     }
 
     public WrappedFancyPlayerWidget setRightHandItem(@Nullable ItemStack item) {
-        this.rightHandItem = copyItem(item);
+        wrapped.setRightHandItem(item);
         return this;
     }
 
     public WrappedFancyPlayerWidget setLeftHandItem(@Nullable Item item) {
-        this.leftHandItem = item == null ? null : ItemStackUtils.createDisplayStack(item);
+        wrapped.setLeftHandItem(item);
         return this;
     }
 
     public WrappedFancyPlayerWidget setLeftHandItem(@Nullable ItemStack item) {
-        this.leftHandItem = copyItem(item);
+        wrapped.setLeftHandItem(item);
         return this;
     }
 
     public WrappedFancyPlayerWidget setHeadWearable(@Nullable String item, HolderLookup.Provider provider) {
-        this.headWearable = null;
+        wrapped.setHeadWearable(item, provider);
         return this;
     }
 
     public WrappedFancyPlayerWidget setChestWearable(@Nullable String item, HolderLookup.Provider provider) {
-        this.chestWearable = null;
+        wrapped.setChestWearable(item, provider);
         return this;
     }
 
     public WrappedFancyPlayerWidget setLegsWearable(@Nullable String item, HolderLookup.Provider provider) {
-        this.legsWearable = null;
+        wrapped.setLegsWearable(item, provider);
         return this;
     }
 
     public WrappedFancyPlayerWidget setFeetWearable(@Nullable String item, HolderLookup.Provider provider) {
-        this.feetWearable = null;
+        wrapped.setFeetWearable(item, provider);
         return this;
     }
 
     public WrappedFancyPlayerWidget setHeadWearable(@Nullable Item item) {
-        this.headWearable = item == null ? null : ItemStackUtils.createDisplayStack(item);
+        wrapped.setHeadWearable(item);
         return this;
     }
 
     public WrappedFancyPlayerWidget setChestWearable(@Nullable Item item) {
-        this.chestWearable = item == null ? null : ItemStackUtils.createDisplayStack(item);
+        wrapped.setChestWearable(item);
         return this;
     }
 
     public WrappedFancyPlayerWidget setLegsWearable(@Nullable Item item) {
-        this.legsWearable = item == null ? null : ItemStackUtils.createDisplayStack(item);
+        wrapped.setLegsWearable(item);
         return this;
     }
 
     public WrappedFancyPlayerWidget setFeetWearable(@Nullable Item item) {
-        this.feetWearable = item == null ? null : ItemStackUtils.createDisplayStack(item);
+        wrapped.setFeetWearable(item);
         return this;
     }
 
     public WrappedFancyPlayerWidget setHeadWearable(@Nullable ItemStack item) {
-        this.headWearable = copyItem(item);
+        wrapped.setHeadWearable(item);
         return this;
     }
 
     public WrappedFancyPlayerWidget setChestWearable(@Nullable ItemStack item) {
-        this.chestWearable = copyItem(item);
+        wrapped.setChestWearable(item);
         return this;
     }
 
     public WrappedFancyPlayerWidget setLegsWearable(@Nullable ItemStack item) {
-        this.legsWearable = copyItem(item);
+        wrapped.setLegsWearable(item);
         return this;
     }
 
     public WrappedFancyPlayerWidget setFeetWearable(@Nullable ItemStack item) {
-        this.feetWearable = copyItem(item);
+        wrapped.setFeetWearable(item);
         return this;
     }
 
     public WrappedFancyPlayerWidget copyPlayer(String profileName) {
+        wrapped.copyPlayer(profileName);
         return this;
     }
 
     public WrappedFancyPlayerWidget copyPlayer(UUID profileId) {
+        wrapped.copyPlayer(profileId);
         return this;
     }
 
     public WrappedFancyPlayerWidget uncopyPlayer() {
+        wrapped.uncopyPlayer();
         return this;
     }
 
     public boolean isCopyingPlayer() {
-        return false;
+        return wrapped.isCopyingPlayer();
     }
 
     @Override
     public boolean isFocusable() {
-        return this.focusable;
+        return false;
     }
 
     @Override
     public void setFocusable(boolean focusable) {
-        this.focusable = focusable;
     }
 
     @Override
     public boolean isNavigatable() {
-        return this.navigatable;
+        return false;
     }
 
     @Override
     public void setNavigatable(boolean navigatable) {
-        this.navigatable = navigatable;
-    }
-
-    @Nullable
-    private static ItemStack copyItem(@Nullable ItemStack stack) {
-        return stack == null ? null : stack.copy();
     }
 }
