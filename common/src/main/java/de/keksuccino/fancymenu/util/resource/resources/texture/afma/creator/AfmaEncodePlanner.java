9025218a2ced13cae8fcb3a3ae5bea0c893b520f
Keksuccino <no@mail.net>
2026-04-14 03:59:09 +0200

    Fix AFMA planner shortlist import

--- common/src/main/java/de/keksuccino/fancymenu/util/resource/resources/texture/afma/creator/AfmaEncodePlanner.java
+++ common/src/main/java/de/keksuccino/fancymenu/util/resource/resources/texture/afma/creator/AfmaEncodePlanner.java
@@ -30,6 +30,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.LinkedHashMap;
