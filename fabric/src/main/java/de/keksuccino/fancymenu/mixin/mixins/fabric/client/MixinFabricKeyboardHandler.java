2812b645ae6c04d63057e4ce24c7a9c002cbb3d7
Keksuccino <no@mail.net>
2026-04-30 03:03:26 +0200

    Porting v3.9.0 from MC 1.20.1 to MC 1.19.2

--- fabric/src/main/java/de/keksuccino/fancymenu/mixin/mixins/fabric/client/MixinFabricKeyboardHandler.java
+++ fabric/src/main/java/de/keksuccino/fancymenu/mixin/mixins/fabric/client/MixinFabricKeyboardHandler.java
@@ -3,6 +3,7 @@ package de.keksuccino.fancymenu.mixin.mixins.fabric.client;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import de.keksuccino.fancymenu.events.screen.ScreenCharTypedEvent;
 import de.keksuccino.fancymenu.util.event.acara.EventHandler;
+import de.keksuccino.fancymenu.util.mcef.WrappedMCEFBrowser;
 import net.minecraft.client.KeyboardHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.events.GuiEventListener;
@@ -37,6 +38,30 @@ public class MixinFabricKeyboardHandler {
         this.cached_modifiers_FancyMenu = modifiers;
     }
 
+    /**
+     * @reason This adds special key press handling for FancyMenu's {@link WrappedMCEFBrowser}.
+     */
+    @WrapWithCondition(method = "keyPress", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;wrapScreenError(Ljava/lang/Runnable;Ljava/lang/String;Ljava/lang/String;)V"))
+    private boolean wrap_keyPressed_keyReleased_in_keyPress_FancyMenu(Runnable runnable, String message, String className) {
+        Minecraft minecraft = Minecraft.getInstance();
+        Screen screen = minecraft.screen;
+        int action = this.cached_action_FancyMenu;
+        if (screen != null) {
+            for (GuiEventListener listener : screen.children()) {
+                if (listener instanceof WrappedMCEFBrowser) {
+                    boolean b = false;
+                    if (action == 1 || action == 2) {
+                        b = listener.keyPressed(this.cached_key_FancyMenu, this.cached_scanCode_FancyMenu, this.cached_modifiers_FancyMenu);
+                    } else if (action == 0) {
+                        b = listener.keyReleased(this.cached_key_FancyMenu, this.cached_scanCode_FancyMenu, this.cached_modifiers_FancyMenu);
+                    }
+                    if (b) return false;
+                }
+            }
+        }
+        return true;
+    }
+
     @Inject(method = "charTyped", at = @At(value = "INVOKE", target = "Ljava/lang/Character;charCount(I)I"))
     private void before_charCount_FancyMenu(long windowPointer, int codePoint, int modifiers, CallbackInfo info) {
         this.cached_char_windowPointer_FancyMenu = windowPointer;
@@ -45,7 +70,8 @@ public class MixinFabricKeyboardHandler {
     }
 
     /**
-     * @reason This handles the CharTypedEvent.
+     * @reason This adds special char typed handling for FancyMenu's {@link WrappedMCEFBrowser}.
+     *         It also handles the CharTypedEvent.
      */
     @WrapWithCondition(method = "charTyped", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;wrapScreenError(Ljava/lang/Runnable;Ljava/lang/String;Ljava/lang/String;)V"))
     private boolean wrap_screen_charTyped_in_charTyped_FancyMenu(Runnable runnable, String message, String className) {
@@ -54,6 +80,21 @@ public class MixinFabricKeyboardHandler {
         if (screen != null) {
             // Fire CharTypedEvent
             EventHandler.INSTANCE.postEvent(new ScreenCharTypedEvent(screen, (char) this.cached_char_codePoint_FancyMenu));
+
+            // Handle browser typing logic
+            for (GuiEventListener listener : screen.children()) {
+                if (listener instanceof WrappedMCEFBrowser) {
+                    boolean b = false;
+                    if (Character.charCount(this.cached_char_codePoint_FancyMenu) == 1) {
+                        b = listener.charTyped((char) this.cached_char_codePoint_FancyMenu, this.cached_char_modifiers_FancyMenu);
+                    } else {
+                        for (char c : Character.toChars(this.cached_char_codePoint_FancyMenu)) {
+                            b = !b ? listener.charTyped(c, this.cached_char_modifiers_FancyMenu) : true;
+                        }
+                    }
+                    if (b) return false;
+                }
+            }
         }
         return true;
     }
