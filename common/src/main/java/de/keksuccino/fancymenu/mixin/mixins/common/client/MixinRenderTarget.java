787ff3f7cc5d2a51b993098ddd90e0721e566f42
Keksuccino <no@mail.net>
2026-05-02 14:15:47 +0200

    Porting v3.9.0 from MC 1.20.1 to MC 1.19.2

--- common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinRenderTarget.java
+++ common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinRenderTarget.java
@@ -7,13 +7,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = RenderTarget.class, remap = false)
+@Mixin(value = RenderTarget.class)
 public class MixinRenderTarget {
 
     /**
      * @reason Preserve caller-controlled blending for RenderTarget blits that explicitly request it.
      */
-    @Inject(method = "_blitToScreen", at = @At("HEAD"))
+    @Inject(method = "_blitToScreen(IIZ)V", at = @At("HEAD"))
     private void before_blitToScreen_FancyMenu(int width, int height, boolean disableBlend, CallbackInfo info) {
         if (!disableBlend) {
             RenderingUtils.assumeOpaqueShaderBlendMode();
