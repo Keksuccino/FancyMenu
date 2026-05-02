7658ee6ea232ef8f2c956646d0847ce2a01ae1c3
Keksuccino <no@mail.net>
2026-05-02 15:42:52 +0200

    Porting v3.9.0 from MC 1.20.1 to MC 1.19.2

--- common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinLevelStorageAccess.java
+++ common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinLevelStorageAccess.java
@@ -8,7 +8,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import java.nio.file.Path;
 
 @Mixin(LevelStorageSource.LevelStorageAccess.class)
 public class MixinLevelStorageAccess {
@@ -16,7 +15,7 @@ public class MixinLevelStorageAccess {
     @Shadow @Final LevelStorageSource.LevelDirectory levelDirectory;
 
     @Inject(method = "<init>", at = @At("TAIL"))
-    private void onConstructFancyMenu(LevelStorageSource $$0, String $$1, Path $$2, CallbackInfo ci) {
+    private void onConstructFancyMenu(LevelStorageSource levelStorageSource, String levelId, CallbackInfo info) {
         LastWorldHandler.setLastWorld(this.levelDirectory.path().toFile().getPath().replace("\\", "/"), false);
     }
 
