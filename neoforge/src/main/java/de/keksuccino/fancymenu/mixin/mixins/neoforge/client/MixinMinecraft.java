7f238e80fae169ffa53b7d9729146d4020ef6b83
Keksuccino <Keksuccino@users.noreply.github.com>
2026-07-12 17:16:24 +0200

    ---

--- neoforge/src/main/java/de/keksuccino/fancymenu/mixin/mixins/neoforge/client/MixinMinecraft.java
+++ neoforge/src/main/java/de/keksuccino/fancymenu/mixin/mixins/neoforge/client/MixinMinecraft.java
@@ -15,14 +15,7 @@ public class MixinMinecraft {
      * @reason NeoForge initializes mods before the client resource infrastructure exists, so FancyMenu
      * initializes once Minecraft is ready enough but before GameLoadCookie starts the customization engine.
      */
-    @Inject(
-            method = "<init>",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/neoforged/neoforge/client/ClientHooks;initClientHooks(Lnet/minecraft/client/Minecraft;Lnet/minecraft/server/packs/resources/ReloadableResourceManager;)V",
-                    shift = At.Shift.AFTER
-            )
-    )
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/neoforged/neoforge/client/ClientHooks;initClientHooks(Lnet/minecraft/client/Minecraft;Lnet/minecraft/server/packs/resources/ReloadableResourceManager;)V", shift = At.Shift.AFTER))
     private void after_initClientHooks_NeoForge_FancyMenu(GameConfig gameConfig, CallbackInfo info) {
 
         FancyMenu.init();
