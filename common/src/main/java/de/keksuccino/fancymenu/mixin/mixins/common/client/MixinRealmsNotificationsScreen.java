75fae613f947d471e7dfebb1a075cfcfa6fb55d0
Keksuccino <Keksuccino@users.noreply.github.com>
2026-07-12 17:21:06 +0200

    ---

--- common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinRealmsNotificationsScreen.java
+++ common/src/main/java/de/keksuccino/fancymenu/mixin/mixins/common/client/MixinRealmsNotificationsScreen.java
@@ -1,19 +1,13 @@
 package de.keksuccino.fancymenu.mixin.mixins.common.client;
 
-import de.keksuccino.fancymenu.accessor.client.IRealmsNotificationsScreenStatics;
 import com.mojang.realmsclient.gui.screens.RealmsNotificationsScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RealmsNotificationsScreen.class)
-public class MixinRealmsNotificationsScreen implements IRealmsNotificationsScreenStatics {
-
-    @Shadow private static boolean trialAvailable;
-    @Shadow private static boolean hasUnreadNews;
-    @Shadow private static boolean hasUnseenNotifications;
+public class MixinRealmsNotificationsScreen {
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void onConstructFancyMenu(CallbackInfo info) {
@@ -24,21 +18,6 @@ public class MixinRealmsNotificationsScreen implements IRealmsNotificationsScree
     @SuppressWarnings("all")
     private RealmsNotificationsScreen getRealmsScreenFancyMenu() {
         return (RealmsNotificationsScreen) ((Object)this);
-    }
-
-    @Override
-    public boolean fancymenu$trialAvailable() {
-        return trialAvailable;
-    }
-
-    @Override
-    public boolean fancymenu$hasUnreadNews() {
-        return hasUnreadNews;
-    }
-
-    @Override
-    public boolean fancymenu$hasUnseenNotifications() {
-        return hasUnseenNotifications;
     }
 
 }
